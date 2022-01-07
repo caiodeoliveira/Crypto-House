8dc87923575c6264787f1a00fb2453d79b41522a
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-01-06 22:28:28 -0300

    🔖 Add Version to Topbar Component.

--- src/components/organisms/LoginUpperArea/index.tsx
+++ src/components/organisms/LoginUpperArea/index.tsx
@@ -1,11 +1,12 @@
 import S from "./styles";
 import TopBar from "../../atoms/TopBar";
-
+import Text from "../../atoms/Text";
 const LoginUpperArea = () => {
   return (
     <>
       <S.TopContainer>
-        <TopBar children={undefined} />
+        <Text type={"input_label"}>{"2.0.0"}</Text>
+        <TopBar></TopBar>
       </S.TopContainer>
     </>
   );
