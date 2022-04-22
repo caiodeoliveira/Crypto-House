4ad1a8dac62965da7aa27cdda8ce3e02de43f3e8
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-04-22 19:54:17 -0300

    📦 Installed typesafe/action

---
+++ src/services/store/ducks/cart/reducer.ts
@@ -0,0 +1,24 @@
+import produce from "immer";
+
+import { CartState, CartTypes } from "./types";
+
+const INITIAL_STATE: CartState = {
+  totalValue: 0,
+  amountValue: 0,
+  loading: false,
+};
+
+export default function cart(state = INITIAL_STATE, action: any = null) {
+  return produce(state, (newState) => {
+    switch (action.type) {
+      case CartTypes.SUM_TOTAL_VALUE: {
+        newState.totalValue + 1;
+        break;
+      }
+      case CartTypes.SUB_TOTAL_VALUE: {
+        newState.totalValue - 1;
+        break;
+      }
+    }
+  });
+}
--- src/services/store/index.ts
+++ src/services/store/index.ts
@@ -1,1 +1,24 @@
-import { applyMiddleware, Store, AnyAction } from "redux";
+// import { configureStore } from "@reduxjs/toolkit/";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  Store,
+  AnyAction,
+  compose,
+} from "redux";
+import createSagaMiddleware from "redux-saga";
+import { CartState } from "./ducks/cart/types";
+
+const sagaMiddleware = createSagaMiddleware();
+
+export interface applicationState {
+  cart: CartState;
+}
+
+const store: Store<unknown, AnyAction> = createStore(
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
