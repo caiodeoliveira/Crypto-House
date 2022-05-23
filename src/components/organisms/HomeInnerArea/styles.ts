269622165f8c08eef3de9b9b3e9276f6538d93b7
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-05-23 12:58:49 -0300

    🚧 Added Coins Container Header

--- src/components/organisms/HomeInnerArea/styles.ts
+++ src/components/organisms/HomeInnerArea/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: row;
   height: 100vh;
   width: 100vw;
   align-items: center;
@@ -10,5 +11,11 @@ export const Container = styled.div`
 
 export const CoinsContainer = styled.div`
   width: 100%;
-  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CoinsContainerHeader = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
