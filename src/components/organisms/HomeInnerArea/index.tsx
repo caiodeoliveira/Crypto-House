f8267f261d7eb2896fdd9d3f6e7f0bedaeb19bb4
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-05-26 17:12:54 -0300

    🎨 Changed code structure to simplify components understading

--- src/components/organisms/HomeInnerArea/index.tsx
+++ src/components/organisms/HomeInnerArea/index.tsx
@@ -1,4 +1,4 @@
-import * as S from "./styles";
+import { S } from "./styles";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { ApplicationState } from "../../../services/store/index";
@@ -22,6 +22,7 @@ export const HomeInnerArea = () => {
   useEffect(() => {
     newCoins && setCoins(newCoins);
     renderInnerArea();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [newCoins]);
 
   const renderInnerArea = () => {
@@ -30,62 +31,80 @@ export const HomeInnerArea = () => {
     } else {
       return coins.map((crypto: Coins, index: number) => {
         return (
-          <S.CoinsListContainer key={index}>
-            <S.CoinsRowContent>
-              <S.MarketCapRankContainer space={"rank"}>
+          <S.general.CoinsListContainer key={index}>
+            <S.body.CoinsRowContent>
+              <S.body.CoinMarketCapRankContainer space={"rank"}>
                 <Text type={"title"}>{crypto.market_cap_rank}</Text>
-              </S.MarketCapRankContainer>
-              <S.CoinNameAndImageContainer space={"coin"}>
-                <S.CoinsImage src={crypto.image} />
+              </S.body.CoinMarketCapRankContainer>
+              <S.body.CoinNameImageAndSymbolContainer space={"coin"}>
+                <S.body.CoinsImage src={crypto.image} />
                 <Text type={"title"}>{crypto.name}</Text>
                 <Text type={"title"}>({crypto.symbol?.toUpperCase()})</Text>
-              </S.CoinNameAndImageContainer>
-              <S.CoinPriceContainer space={"price"}>
+              </S.body.CoinNameImageAndSymbolContainer>
+              <S.body.CoinPriceContainer space={"price"}>
                 <Text type={"title"}>
                   ${crypto.current_price?.toLocaleString("en-US")}
                 </Text>
-              </S.CoinPriceContainer>
-              <S.CoinPercentageDayContainer space={"price_change_day"}>
+              </S.body.CoinPriceContainer>
+              <S.body.CoinPercentageDayContainer space={"price_change_day"}>
                 <Text type={"title"}>
                   {crypto.price_change_percentage_24h?.toFixed(2)}%
                 </Text>
-              </S.CoinPercentageDayContainer>
-              <S.CoinPercentageWeekContainer space={"price_change_week"}>
+              </S.body.CoinPercentageDayContainer>
+              <S.body.CoinPercentageWeekContainer space={"price_change_week"}>
                 <Text type={"title"}>
                   {crypto.price_change_percentage_7d_in_currency?.toFixed(2)}%
                 </Text>
-              </S.CoinPercentageWeekContainer>
-              <S.CoinMarketCapValueContainer space={"market_cap_value"}>
+              </S.body.CoinPercentageWeekContainer>
+              <S.body.CoinMarketCapValueContainer space={"market_cap_value"}>
                 <Text type={"title"}>{crypto.market_cap?.toFixed(2)}</Text>
-              </S.CoinMarketCapValueContainer>
-              <S.CoinVolumeContainer space={"volume"}>
+              </S.body.CoinMarketCapValueContainer>
+              <S.body.CoinVolumeContainer space={"volume"}>
                 <Text type={"title"}>{crypto.total_volume?.toFixed(2)}%</Text>
-              </S.CoinVolumeContainer>
-              <S.CoinCirculatingSupplyContainer space={"circulating_supply"}>
+              </S.body.CoinVolumeContainer>
+              <S.body.CoinCirculatingSupplyContainer
+                space={"circulating_supply"}
+              >
                 <Text type={"title"}>
                   {crypto.circulating_supply?.toFixed(2)}%
                 </Text>
-              </S.CoinCirculatingSupplyContainer>
-            </S.CoinsRowContent>
-          </S.CoinsListContainer>
+              </S.body.CoinCirculatingSupplyContainer>
+            </S.body.CoinsRowContent>
+          </S.general.CoinsListContainer>
         );
       });
     }
   };
   return (
-    <S.Container>
-      <S.CoinsContainerHeader>
-        <Text type={"title"}>#</Text>
-        <Text type={"title"}>Coin</Text>
-        <Text type={"title"}>Symbol</Text>
-        <Text type={"title"}>Price</Text>
-        <Text type={"title"}>24h %</Text>
-        <Text type={"title"}>7d %</Text>
-        <Text type={"title"}>Market Cap</Text>
-        <Text type={"title"}>Volume(24h)</Text>
-        <Text type={"title"}>Circulating Supply</Text>
-      </S.CoinsContainerHeader>
+    <S.general.Container>
+      <S.general.CoinsContainerHeader>
+        <S.Header.CoinMarketCapRankHeaderContainer space={"rank"}>
+          <Text type={"title"}>#</Text>
+        </S.Header.CoinMarketCapRankHeaderContainer>
+        <S.Header.CoinNameImageAndSymbolHeaderContainer space={"coin"}>
+          <Text type={"title"}>Coin</Text>
+          <Text type={"title"}>Symbol</Text>
+        </S.Header.CoinNameImageAndSymbolHeaderContainer>
+        <S.Header.CoinPriceHeaderContainer space={"price"}>
+          <Text type={"title"}>Price</Text>
+        </S.Header.CoinPriceHeaderContainer>
+        <S.Header.CoinPercentageDayHeaderContainer space={"price_change_day"}>
+          <Text type={"title"}>24h %</Text>
+        </S.Header.CoinPercentageDayHeaderContainer>
+        <S.Header.CoinPercentageWeekHeaderContainer space={"price_change_week"}>
+          <Text type={"title"}>7d %</Text>
+        </S.Header.CoinPercentageWeekHeaderContainer>
+        <S.Header.CoinMarketCapValueHeaderContainer space={"market_cap_value"}>
+          <Text type={"title"}>Market Cap</Text>
+        </S.Header.CoinMarketCapValueHeaderContainer>
+        <S.Header.CoinVolumeHeaderContainer space={"volume"}>
+          <Text type={"title"}>Volume(24h)</Text>
+        </S.Header.CoinVolumeHeaderContainer>
+        <S.Header.CoinCirculatingSupplyContainer space={"circulating_supply"}>
+          <Text type={"title"}>Circulating Supply</Text>
+        </S.Header.CoinCirculatingSupplyContainer>
+      </S.general.CoinsContainerHeader>
       {renderInnerArea()}
-    </S.Container>
+    </S.general.Container>
   );
 };
--- src/components/organisms/HomeInnerArea/styles.ts
+++ src/components/organisms/HomeInnerArea/styles.ts
@@ -14,12 +14,70 @@ export const CoinsContainerHeader = styled.header`
   height: 100%;
   display: flex;
   flex-direction: row;
+  justify-content: center;
   position: sticky;
   top: 0;
   background-color: blue;
   margin-bottom: 20px;
 `;
+/* Header Container Components */
+export const CoinMarketCapRankHeaderContainer = styled.div<HomeInnerAreaProps>`
+  width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
+  display: flex;
+  align-items: center;
+  justify-content: left;
+`;
 
+export const CoinNameImageAndSymbolHeaderContainer = styled.div<HomeInnerAreaProps>`
+  width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  gap: 120px;
+`;
+
+export const CoinPriceHeaderContainer = styled.div<HomeInnerAreaProps>`
+  width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CoinPercentageDayHeaderContainer = styled.div<HomeInnerAreaProps>`
+  width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CoinPercentageWeekHeaderContainer = styled.div<HomeInnerAreaProps>`
+  width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CoinMarketCapValueHeaderContainer = styled.div<HomeInnerAreaProps>`
+  width: ${(props) => handleSpaceInnerAreaContainer(props.space)} + 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CoinVolumeHeaderContainer = styled.div<HomeInnerAreaProps>`
+  width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
+  display: flex;
+  align-items: center;
+  justify-content: right;
+`;
+
+export const CoinCirculatingSupplyHeaderContainer = styled.div<HomeInnerAreaProps>`
+  width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
+  display: flex;
+  align-items: center;
+  justify-content: right;
+`;
+/* Body Container Components */
 export const CoinsListContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -38,7 +96,7 @@ export const CoinsRowContent = styled.div`
   margin-bottom: 30px;
 `;
 
-export const MarketCapRankContainer = styled.div<HomeInnerAreaProps>`
+export const CoinMarketCapRankContainer = styled.div<HomeInnerAreaProps>`
   height: 100%;
   width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
   display: flex;
@@ -46,26 +104,18 @@ export const MarketCapRankContainer = styled.div<HomeInnerAreaProps>`
   justify-content: left;
 `;
 
-export const CoinNameAndImageContainer = styled.div<HomeInnerAreaProps>`
+export const CoinNameImageAndSymbolContainer = styled.div<HomeInnerAreaProps>`
   width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: left;
-  gap: 10px;
+  gap: 20px;
 `;
 
 export const CoinsImage = styled.img`
   max-width: 60px;
   max-height: 60px;
-`;
-
-export const CoinSymbolContainer = styled.div<HomeInnerAreaProps>`
-  width: ${(props) => handleSpaceInnerAreaContainer(props.space)};
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
 
 export const CoinPriceContainer = styled.div<HomeInnerAreaProps>`
@@ -121,15 +171,15 @@ const handleSpaceInnerAreaContainer = (space: string) => {
     case "market_cap_rank":
       return "2%";
     case "coin":
-      return "23%";
+      return "17%";
     case "price":
       return "10%";
     case "price_change_day":
       return "8%";
     case "price_change_week":
-      return "8%";
+      return "16%";
     case "market_cap":
-      return "17%";
+      return "15%";
     case "volume":
       return "17%";
     case "circulating_supply":
@@ -137,4 +187,35 @@ const handleSpaceInnerAreaContainer = (space: string) => {
     default:
       return "5%";
   }
+};
+
+export const S = {
+  general: {
+    Container,
+    CoinsListContainer,
+    CoinsContainerHeader,
+  },
+
+  Header: {
+    CoinMarketCapRankHeaderContainer,
+    CoinNameImageAndSymbolHeaderContainer,
+    CoinPriceHeaderContainer,
+    CoinPercentageDayHeaderContainer,
+    CoinPercentageWeekHeaderContainer,
+    CoinMarketCapValueHeaderContainer,
+    CoinVolumeHeaderContainer,
+    CoinCirculatingSupplyContainer,
+  },
+  body: {
+    CoinsRowContent,
+    CoinMarketCapRankContainer,
+    CoinNameImageAndSymbolContainer,
+    CoinPriceContainer,
+    CoinsImage,
+    CoinPercentageDayContainer,
+    CoinPercentageWeekContainer,
+    CoinMarketCapValueContainer,
+    CoinVolumeContainer,
+    CoinCirculatingSupplyContainer,
+  },
 };
